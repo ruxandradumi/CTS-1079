76a6838c738e8a1a3ac3b6009772e2e9abe86ffc
ruxandradumitrescu24 <ruxandradumitrescu24@yahoo.com>
2021-03-11 08:10:29 +0200

    seminar 2

--- seminar2/src/ro/ase/cts/classes/Aplicant.java
+++ seminar2/src/ro/ase/cts/classes/Aplicant.java
@@ -59,8 +59,12 @@ public abstract class Aplicant{
 	public int getNr_proiecte() {
 		return nr_proiecte;
 	}
-	public void setNr_proiecte(int nr_proiecte) {
+	public void setNr_proiecte(int nr_proiecte, String[] vect) {
 		this.nr_proiecte = nr_proiecte;
+		for(int i = 0; i < nr_proiecte; i++) {
+			this.denumireProiect = vect;
+		}
+			
 	}
 
 }
--- seminar2/src/ro/ase/cts/readere/AplicantReader.java
+++ seminar2/src/ro/ase/cts/readere/AplicantReader.java
@@ -1,24 +1,47 @@
 package ro.ase.cts.readere;
 
-import java.io.FileNotFoundException;
-import java.util.List;
-
 import ro.ase.cts.classes.Aplicant;
 
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Scanner;
+
 public abstract class AplicantReader {
-	
 	private String fileName;
+	
+	public abstract List<Aplicant> citesteAplicanti() throws FileNotFoundException, NumberFormatException;
+
+	public Aplicant citesteAplicant(Scanner input, Aplicant aplicant) {
+		String nume = input.next();
+		String prenume = input.next();
+		int varsta = input.nextInt();
+		int punctaj = input.nextInt();
+		int nr = input.nextInt();
+		String[] vect = new String[5];
+		for (int i = 0; i < nr; i++)
+			vect[i] = input.next();
+		
+		aplicant.setNume(nume);
+		aplicant.setPrenume(prenume);
+		aplicant.setVarsta(varsta);
+		aplicant.setNr_proiecte(nr, vect);
+		aplicant.setPunctaj(punctaj);
+		
+		return aplicant;
+	}
 	
 	public AplicantReader(String fileName) {
 		super();
 		this.fileName = fileName;
 	}
-	
-	
+
 	public String getFileName() {
 		return fileName;
 	}
 
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
 
-	public abstract List<Aplicant> citesteAplicanti() throws FileNotFoundException, NumberFormatException;
+	
 }
