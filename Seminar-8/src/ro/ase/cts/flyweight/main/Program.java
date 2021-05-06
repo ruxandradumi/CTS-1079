5cd416fa54f79ccdffd069e1545af854eb0414ce
ruxdumi <dumitrescuruxandra24@stud.ase.ro>
2021-05-06 10:21:42 +0300

    main

--- Seminar-8/src/ro/ase/cts/flyweight/main/Program.java
+++ Seminar-8/src/ro/ase/cts/flyweight/main/Program.java
@@ -1,4 +1,19 @@
 package ro.ase.cts.flyweight.main;
 
+import ro.ase.cts.flyweight.classes.FabricaRezervari;
+import ro.ase.cts.flyweight.classes.Rezervare;
+
 public class Program {
+
+    public static void main(String[] args) {
+        Rezervare rezervare1 = new Rezervare(1, 3, "12.00");
+        Rezervare rezervare2 = new Rezervare(3, 5, "16.00");
+
+        FabricaRezervari fabrica = new FabricaRezervari();
+        fabrica.getClient("alisa@email.com").afisareInformatii(rezervare1);
+        fabrica.getClient("alisa@email.com").afisareInformatii(rezervare2);
+        Rezervare rezervare3 = new Rezervare(2, 4, "17.00");
+        fabrica.getClient("email").afisareInformatii(rezervare3);
+    }
+
 }
